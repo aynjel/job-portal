9432ba0c4e0f6850a6468add27167f6f448791e6
anggi <ortegacanillo76@gmail.com>
2024-05-03 17:23:29 +0800

    revert and modify base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/job-portal/',
+  base: "https://aynjel.github.io/job-portal/",
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
   server: {
     port: 3001,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
+      "/api": {
+        target: "http://localhost:5001",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});

27d7aa3529ddc6afa6ee7eca896dfd7a118a7431
anggi <ortegacanillo76@gmail.com>
2024-05-03 17:46:25 +0800

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://aynjel.github.io/job-portal/",
+  base: "/job-portal/",
   build: {
     outDir: "dist",
   },
